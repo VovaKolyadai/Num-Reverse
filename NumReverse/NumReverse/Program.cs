3b7075117fb3a1140546100eee05283f832c38c2
Koliada Volodymyr <medzhigam.games@gmail.com>
2024-01-21 09:18:44 +0100

    Create c# project

---
+++ NumReverse/NumReverse/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NumReverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reverse this num!");
+        }
+    }
+}
